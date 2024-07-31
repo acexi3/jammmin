ae848ba2d4479c1fe46f37db16e1a8c819f632d5
Douglas Baker <acexi3@protonmail.com>
2024-07-31 10:27:14 -0400

    Created component structure and started NavBar & Hero components.

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hero.css';
+
+export default function Hero() {
+    return (
+        <div className="Hero">
+            <h1>Looking for Music?</h1>
+            <p>Create or otherwise manipulate your Spotify playlists, here!</p>
+        </div>
+    );
+};
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar() {
+    return (
+      <header className="NavBar">
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Profile</li>
+          </ul>
+        </nav>
+      </header>
+    );
+};
