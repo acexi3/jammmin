ea2b1e61e2038b7d615f895d1575723987d5fe3f
Douglas Baker <acexi3@protonmail.com>
2024-08-03 11:40:37 -0400

    Moved Connector functional C into Hero functional C, installed spotify-web-api-js library

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Connector from '../Connector/Connector';
 import './Hero.css';
 
 export default function Hero() {
     return (
         <div className="Hero">
             <h1>Looking for Music?</h1>
-            <p>Create or otherwise manipulate your Spotify playlists, here!</p>
+            <p>Create or otherwise manipulate your Spotify playlists, here.</p><br/>
+            <Connector /> 
         </div>
         
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import NavBar from '../components/NavBar/NavBar';
 import Hero from '../components/Hero/Hero';
-import Connector from '../components/Connector/Connector';
 
 export default function Home() {
     return (
@@ -10,7 +9,6 @@ export default function Home() {
                 <div className="Home">    
                     {/*<img src={logo} className="App-logo" alt="logo" />*/}
                     <Hero />
-                    <Connector /> 
                 </div>
             </div>
         </>    
